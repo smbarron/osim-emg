977847e6df7ff2bc2665f6e7493a72bac0972456
Sarah Barron <91490621+smbarron@users.noreply.github.com>
2023-11-22 12:38:10 -0500

    docstrings, typehints, and cleanup

--- activations_to_states.py
+++ activations_to_states.py
@@ -2,7 +2,8 @@
 import pandas as pd
 
 
-def combine_files(states_file_path):
+def combine_files(states_file_path: str) -> str:
+    """Wrapper to insert the measured activations into the states file and save it."""
     states_df = load_states_file(states_file_path)
 
     activation_file_path = states_file_path.replace("StatesReporter_states", "StaticOptimization_activation")
@@ -16,33 +17,37 @@ def combine_files(states_file_path):
     return output_filename
 
 
-def load_activation_file(filename):
+def load_activation_file(filename: str) -> pd.DataFrame:
+    """Load the activation file as a pandas dataframe."""
     return pd.read_table(filename, skiprows=8, header=0, delimiter="\t")
 
 
-def load_states_file(filename):
+def load_states_file(filename: str) -> pd.DataFrame:
+    """Load the states file as a pandas dataframe."""
     return pd.read_table(filename, skiprows=12, header=0, delimiter="\t")
 
 
-def merge_files(activation_df, states_df):
-    activation_columns = [col for col in activation_df.columns if not col.endswith("_res") and col != "time"]
+def merge_files(activations: pd.DataFrame, states: pd.DataFrame) -> pd.DataFrame:
+    """Merge the activations and states."""
+    activation_columns = [col for col in activations.columns if not col.endswith("_res") and col != "time"]
     states_columns_to_replace = [f"/forceset/{col}/activation" for col in activation_columns]
 
-    combined_df = states_df.copy()
+    combined_df = states.copy()
 
     for act_col, state_col in zip(activation_columns, states_columns_to_replace):
         if state_col in combined_df.columns:
-            combined_df[state_col] = activation_df[act_col]
+            combined_df[state_col] = activations[act_col]
 
     return combined_df
 
 
-def save_combined_file(df, filename):
+def save_combined_file(data: pd.DataFrame, filename: str):
+    """Add the OpenSim header to the combined states file and save it."""
     with open(filename, "w") as f:
         f.write("ModelStates\n")
         f.write("version=1\n")
-        f.write(f"nRows={len(df)}\n")
-        f.write(f"nColumns={len(df.columns)}\n")
+        f.write(f"nRows={len(data)}\n")
+        f.write(f"nColumns={len(data.columns)}\n")
         f.write("inDegrees=no\n")
         f.write("\n")
         f.write("This file contains the states of a model during a simulation.\n")
@@ -52,4 +57,4 @@ def save_combined_file(df, filename):
         f.write("\n")
         f.write("endheader\n")
 
-    df.to_csv(filename, mode="a", sep="\t", index=False, float_format="%.8f")
+    data.to_csv(filename, mode="a", sep="\t", index=False, float_format="%.8f")
--- merge_measured_with_predicted.py
+++ merge_measured_with_predicted.py
@@ -1,15 +1,17 @@
 """Functions to merge measured EMG with results from SO simulations for non-measured muscles."""
 import os
+import pickle
+from typing import Dict, Optional, Union
 
 import pandas as pd
 
 from osim_emg.activations_to_states import load_activation_file
 from osim_emg.constants import EMG_SAMPLING_RATE, MOCAP_SAMPLING_RATE, MUSCLE_MAPPING
-from osim_emg.data import COMB_EMG_DATA, SPLIT_EMG_DATA
 
 
-def save_combined_activations(activation_path, tear):
-    activation_data_new = combine_activations(activation_path, tear)
+def save_combined_activations(activation_path: str, split_emg_filepath: str, not_split_emg_filepath: str, tear: Optional[bool]) -> str:
+    """Wrapper to combine measured and predicted activations and save them to a file."""
+    activation_data_new = combine_activations(activation_path, split_emg_filepath, not_split_emg_filepath, tear)
     output_filename = activation_path.replace("activation", "CombinedActivations")
 
     save_combined_activation_file(activation_data_new, output_filename)
@@ -17,7 +19,8 @@ def save_combined_activations(activation_path, tear):
     return output_filename
 
 
-def add_muscle_to_mapping(muscle_map, tear):
+def add_muscle_to_mapping(muscle_map: Dict[str, Union[str, None]], tear: Optional[bool]) -> Dict[str, Union[str, None]]:
+    """Add muscles to the mapping that are measured but only in the model for participatns without a tear."""
     if not tear:
         muscle_map["Supraspinatus_P"] = "Supraspinatus"
         muscle_map["Supraspinatus_A"] = "Supraspinatus"
@@ -25,27 +28,40 @@ def add_muscle_to_mapping(muscle_map, tear):
     return muscle_map
 
 
-def downsample_emg(emg_data, emg_frame_rate, mocap_frame_rate):
+def downsample_emg(emg_data: pd.DataFrame, emg_frame_rate: int, mocap_frame_rate: int) -> pd.DataFrame:
+    """Downsample the EMG data to the same frequency as the motion capture data."""
     sampling_ratio = int(emg_frame_rate / mocap_frame_rate)
     downsampled = emg_data.groupby("Frame").apply(lambda group: group.iloc[::sampling_ratio].mean()).reset_index(drop=True)
 
     return downsampled
 
 
-def save_combined_activation_file(df, filename):
+def save_combined_activation_file(data: pd.DataFrame, filename: str):
+    """Add the OpenSim header to the combined activation file and save it."""
     with open(filename, "w") as f:
         f.write("Static Optimization\n")
         f.write("version=1\n")
-        f.write(f"nRows={len(df)}\n")
-        f.write(f"nColumns={len(df.columns)}\n")
+        f.write(f"nRows={len(data)}\n")
+        f.write(f"nColumns={len(data.columns)}\n")
         f.write("inDegrees=no\n")
         f.write("This file contains static optimization results\n")
         f.write("endheader\n")
 
-    df.to_csv(filename, mode="a", sep="\t", index=False, float_format="%.8f")
+    data.to_csv(filename, mode="a", sep="\t", index=False, float_format="%.8f")
 
 
-def combine_activations(activation_path, tear):
+def load_emg_data(split_filepath: str, notsplit_filepath: str):
+    """Load the measured EMG data from pickle files."""
+    with open(split_filepath, "rb") as file:
+        split_emg_data = pickle.load(file)
+    with open(notsplit_filepath, "rb") as file:
+        comb_emg_data = pickle.load(file)
+
+    return split_emg_data, comb_emg_data
+
+
+def combine_activations(activation_path: str, split_emg_filepath: str, not_split_emg_filepath: str, tear: Optional[bool]) -> pd.DataFrame:
+    """Load the measured and predicted activations and combine them."""
     activation_data = load_activation_file(activation_path)
 
     path_parts = os.path.normpath(activation_path).split(os.sep)
@@ -54,8 +70,9 @@ def combine_activations(activation_path, tear):
     trial_index = trial_full[::-1].find("_OS_") + 4
     trial = trial_full[0:-trial_index]
 
-    measured_data = COMB_EMG_DATA[(subject, trial)]
-    measured_data_cleaned = pd.concat([SPLIT_EMG_DATA[(subject, trial)][i] for i in range(3)], axis=0)
+    split_emg_data, comb_emg_data = load_emg_data(split_emg_filepath, not_split_emg_filepath)
+    measured_data = comb_emg_data[(subject, trial)]
+    measured_data_cleaned = pd.concat([split_emg_data[(subject, trial)][i] for i in range(3)], axis=0)
 
     merged = measured_data.where(measured_data_cleaned.notnull())
     merged_new = merged.dropna(axis=0, how="all")
